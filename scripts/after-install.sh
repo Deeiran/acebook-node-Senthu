cafd14f18387e6b1ed894369c4bb0f2a4f3a3c07
Senthooran Vyravippillai <senthu_26@yahoo.com>
2023-11-14 15:51:03 +0000

    updated ater install file

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/acebook
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash   
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 18
+nvm use 18
 npm install
